ba3adc82a547b150aa9427cb03cc559844270b69
thepeterkovacs <peterkov37@gmail.com>
2023-09-05 19:45:18 +0200

    remove nullish coalescing from auth route

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,8 +4,8 @@ import GitHubProvider from "next-auth/providers/github";
 export const authOptions = {
   providers: [
     GitHubProvider({
-      clientId: process.env.GITHUB_ID ?? "",
-      clientSecret: process.env.GITHUB_SECRET ?? "",
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
 };
